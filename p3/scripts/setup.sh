10cbe2771946052bce005eb169465178116d2ba0
zoldycksh <erwan94@protonmail.com>
2022-04-12 03:17:18 +0200

    fix p3

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -1,3 +1,23 @@
+# install docker
+echo "installing docker"
+
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+apt-get install -y docker-ce
+
+
+# install k3d
+echo "installing k3d"
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+
+#install kubectl
+echo "installing kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
 k3d cluster create part3 -p 8080:80@loadbalancer -p 8443:443@loadbalancer -p 4242:3006
 kubectl create namespace argocd
 kubectl create namespace dev
